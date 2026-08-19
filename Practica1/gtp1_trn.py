3475302df17a84291fbc6a0cb78db2980d45c703
Log_4n <jorgepalacios6422@gmail.com>
2026-08-19 15:06:17 -0300

    no me acuerdo

--- Practica1/gtp1_trn.py
+++ Practica1/gtp1_trn.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 it = 0;
 epocaMax = 5;
-datos = pd.read_csv("XOR_trn.csv");
+datos = pd.read_csv("OR_50_trn.csv");
 datos["bias"] = -1;
 porcentaje_aciertos = 0;
 pesos = np.random.uniform(-0.5,0.5,3);
@@ -14,8 +14,25 @@ cantidad_entradas = len(datos);
 eta = 0.005;
 print("Pesos iniciales:", pesos)
 
-plt.ion(); #modo interactivo?
+plt.ion(); #modo interactivo
 fig, ax = plt.subplots();
+
+ax.set_title("Entrenamiento")
+ax.set_xlabel("x1")
+ax.set_ylabel("x2")
+ax.set_xlim(-2, 2)
+ax.set_ylim(-2, 2)
+
+ax.axhline(0)
+ax.axvline(0)
+datos_pos = datos[datos.iloc[:, 2] == 1];
+datos_neg = datos[datos.iloc[:, 2] == -1];
+ax.scatter(datos_pos.iloc[:, 0], datos_pos.iloc[:, 1]);
+ax.scatter(datos_neg.iloc[:, 0], datos_neg.iloc[:, 1]);
+x1 = np.linspace(-2, 2, 100);
+x2 = (pesos[2]/pesos[1]) - (pesos[0]/pesos[1])*x1;
+linea, = ax.plot(x1, x2, color="red");
+
 
 while (it < epocaMax and porcentaje_aciertos < 90):
     sum = 0;
@@ -27,27 +44,24 @@ while (it < epocaMax and porcentaje_aciertos < 90):
         y = 1 if z>=0 else -1;
 
         error = fila.iloc[2] - y; 
-        pesos = pesos + (2*eta)*error*entrada;
+        pesos = pesos + (eta/2)*error*entrada;
 
+        if i % 10 == 0:
+            x2 = (pesos[2]/pesos[1]) - (pesos[0]/pesos[1])*x1;
+            linea.set_ydata(x2);
+
+            fig.canvas.draw_idle();
+            fig.canvas.flush_events();
+            plt.pause(0.001);
+    
     #grafico una vez por epoca
-    ax.clear();
-    ax.set_title("Entrenamiento");
-    ax.set_xlabel("x1");
-    ax.set_ylabel("x2");
-    ax.axhline(0);
-    ax.axvline(0);
-    ax.set_xlim(-2, 2);
-    ax.set_ylim(-2, 2);
-
-    x1 = np.linspace(-2, 2, 100);
-    x2 = (pesos[2]/pesos[1]) - (pesos[0]/pesos[1])*x1;
-
-    datos_pos = datos[datos.iloc[:, 2] == 1];
-    datos_neg = datos[datos.iloc[:, 2] == -1];
-    ax.scatter(datos_pos.iloc[:, 0], datos_pos.iloc[:, 1]);
-    ax.scatter(datos_neg.iloc[:, 0], datos_neg.iloc[:, 1]);
-    ax.plot(x1, x2, color="red");
-    plt.pause(0.3);     
+    # x1 = np.linspace(-2, 2, 100);
+    # x2 = (pesos[2]/pesos[1]) - (pesos[0]/pesos[1])*x1;
+    # linea.set_ydata(x2)
+    # fig.canvas.draw();
+    # fig.canvas.flush_events();
+    # plt.pause(0.1);
+    # plt.pause(0.3);     
 
 
     for i in range(cantidad_entradas):
@@ -65,9 +79,9 @@ while (it < epocaMax and porcentaje_aciertos < 90):
     print("Época:", it, "Aciertos:", sum, "Porcentaje:", porcentaje_aciertos);
     it += 1;
     
-print("Pesos finales:", pesos)
+print("Pesos finales:", pesos);
 pesos_df = pd.DataFrame([pesos]);
-pesos_df.to_csv("pesos.csv",index=False)
+pesos_df.to_csv("pesos.csv",index=False);
 
 plt.ioff();
-plt.show();
+plt.show(); 
